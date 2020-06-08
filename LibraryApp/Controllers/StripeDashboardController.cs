6f6c6ff24fb660d50dfa73a0228c9e82a9ef3c20
Dragica <dragican@live.com>
2020-06-08 23:24:57 +0200

    Stripe integrated

---
+++ LibraryApp/Controllers/StripeDashboardController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryApp.Data.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Stripe;
+
+namespace LibraryApp.Controllers
+{
+    public class StripeDashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            var balanceService = new BalanceService();
+            var balanceResult = balanceService.Get();
+            var transactionService = new BalanceTransactionService();
+            var transactionResult = transactionService.List();
+            var customerService = new CustomerService();
+            var customerResult = customerService.List();
+            var chargeService = new ChargeService();
+            var chargeResult = chargeService.List();
+            var disputeService = new DisputeService();
+            var disputeResult = disputeService.List();
+            var refundService = new RefundService();
+            var refundResult = refundService.List();
+            var productService = new ProductService();
+            var productResult = productService.List();
+
+            var result = new StripeDashboardVM()
+            {
+                Balance = balanceResult,
+                Transactions = transactionResult.ToList(),
+                Customers = customerResult.ToList(),
+                Charges = chargeResult.ToList(),
+                Disputes = disputeResult.ToList(),
+                Refunds = refundResult.ToList(),
+                Products = productResult.ToList()
+            };
+            return View(result);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# BookJack-Stripe
+ 
